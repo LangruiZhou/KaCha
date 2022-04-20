0af43a0ebbd5d641624a0a6f440fd89a2d6e06af
zlr_git <794583971@qq.com>
2022-04-20 23:51:28 +0800

    update

---
+++ ARmain.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AR
+//
+//  Created by 苗乔伟 on 2018/9/7.
+//  Copyright © 2018 苗乔伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ARAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ARAppDelegate class]));
+    }
+}
